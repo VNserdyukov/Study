22371bf73a9675183d793ddc5112653fa41f040c
Victor <vn.serdyukov@mail.ru>
2025-04-02 09:31:19 +0300

    Study1

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,2 +1,11 @@
-public class Lesson2 {
-}
+import java.util.Scanner;
+
+    public class Lesson2{
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            double x = scan.nextDouble();
+            x = Math.log(x + 7 * Math.sqrt(Math.pow(x,4) + Math.PI));
+            System.out.printf("%.4f", x);
+        }
+    }
+
