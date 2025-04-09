efcce2e35695c1d6d41cea8477dde9532e03d931
Victor <vn.serdyukov@mail.ru>
2025-04-09 16:37:13 +0300

    Study1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(20367%3600);
+        Scanner scan = new Scanner(System.in);
+        String month = scan.next();
+        switch (month) {
+            case "Декабрь", "Январь", "Февраль" -> System.out.println("Зима");
+            case "Март", "Апрель", "Май" -> System.out.println("Весна");
+            case "Июнь", "Июль", "Август" -> System.out.println("Лето");
+            case "Сентябрь", "Октябрь", "Ноябрь" -> System.out.println("Осень");
+            default -> System.out.println("Неверное название месяца!");
+        }
     }
 }
