a8e173d4a3d28b546b69b43ff4971dbe6187bd4a
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-04-09 19:43:57 +0300

    Lesson22Switch04

---
+++ src/Lesson23Cycle02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lesson23Cycle02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int dan1 = scan.nextInt();
+        int dan2 = scan.nextInt();
+        int dan3;
+        double multi = 1.;
+        dan3 = Math.max(dan1, dan2);
+        dan2 = Math.min(dan1, dan2);
+        while (dan2 <= dan3) {
+            multi *= dan2;
+            dan2++;
+        }
+        System.out.println(multi);
+    }
+}
