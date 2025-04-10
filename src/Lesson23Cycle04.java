e4c1f7b50067f8bfd66e97fa0c30f3f03000d601
Victor <vn.serdyukov@mail.ru>
2025-04-10 16:17:13 +0300

    Study1

---
+++ src/Lesson23Cycle04.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Lesson23Cycle04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int dan = scan.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= dan; i++) {
+            if (dan % i == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
