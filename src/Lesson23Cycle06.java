aab362240c622b25258476f37f24f5ab39e756aa
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-04-10 21:59:24 +0300

    Lesson22Switch04

---
+++ src/Lesson23Cycle06.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Lesson23Cycle06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int dan = scan.nextInt();
+        int sum = 0;
+        int kol = 0;
+        do {
+            sum += (dan % 10);
+            dan = dan / 10;
+            kol++;
+        } while (dan != 0);
+        System.out.println(kol + " " + Math.abs(sum));
+    }
+}
