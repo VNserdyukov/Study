b56498f4e8e9e30203eae848f140c4ea524d1616
Victor <vn.serdyukov@mail.ru>
2025-04-10 15:35:00 +0300

    Study1

---
+++ src/Lesson23Cycle03.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Lesson23Cycle03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int dan = scan.nextInt();
+        int min = scan.nextInt();
+        for (int i = 1; i < dan; i++) {
+            int number = scan.nextInt();
+            min = Math.min(number, min);
+        }
+        System.out.println(min);
+    }
+}
