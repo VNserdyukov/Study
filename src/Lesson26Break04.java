c0349c7ae9095ccd1782bd66e1e858a5c0b35c56
Victor <vn.serdyukov@mail.ru>
2025-04-16 21:54:33 +0300

    Study1

--- src/Lesson26Break04.java
+++ src/Lesson26Break04.java
@@ -3,29 +3,14 @@ import java.util.Scanner;
 public class Lesson26Break04 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int dan1 = scan.nextInt();
-        int dan2 = scan.nextInt();
-        int dan3 = Math.min(dan1, dan2);
-        int dan4 = Math.max(dan1, dan2);
+        int dan = scan.nextInt();
         int sum = 0;
-        for (int i = dan3; i <= dan4; i++) {
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    sum++;
-                }
-                if (sum > 2) {
-                    System.out.println(i);
-                    break;
-                }
-            }
-            if (sum <= 2) {
-                sum = 0;
-            } else {
-                break;
+        for (int i = 2; i < dan; i++) {
+            if (i % 2 != 0) continue;
+            if (dan % i == 0) {
+                sum += i;
             }
         }
-        if (sum == 0) {
-            System.out.println("NO");
-        }
+        System.out.println(sum);
     }
 }
