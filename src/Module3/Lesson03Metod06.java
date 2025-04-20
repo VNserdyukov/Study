0b1456c3c9716e1e1316eaf1ab45139cf31704a8
Victor <vn.serdyukov@mail.ru>
2025-04-20 22:17:05 +0300

    Study1

---
+++ src/Module3/Lesson03Metod06.java
@@ -0,0 +1,31 @@
+package Module3;
+
+import java.util.Scanner;
+
+public class Lesson03Metod06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        double b = scan.nextInt();
+        double c = scan.nextInt();
+        square(a, b);
+        square(a, b, c);
+    }
+
+    static void square(int a, double b) {
+
+        }
+
+
+    static void square(int a, double b, double c) {
+
+        }
+
+
+}
+
+
+
+
+
+
