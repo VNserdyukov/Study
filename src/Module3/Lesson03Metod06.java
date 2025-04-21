36161a2035ee7be37534e124cc2c26d5307109dc
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-04-22 00:27:33 +0300

    Lesson22Switch04

--- src/Module3/Lesson03Metod06.java
+++ src/Module3/Lesson03Metod06.java
@@ -5,22 +5,28 @@ import java.util.Scanner;
 public class Lesson03Metod06 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        double b = scan.nextInt();
-        double c = scan.nextInt();
-        square(a, b);
-        square(a, b, c);
+        int count = scan.nextInt();
+
+        switch (count) {
+            case (1):
+                double a = scan.nextDouble();
+                System.out.printf("%.2f", square(a));
+                break;
+            case (2):
+                double b = scan.nextDouble();
+                double c = scan.nextDouble();
+                System.out.printf("%.2f", square(b, c));
+                break;
+        }
     }
 
-    static void square(int a, double b) {
+    static double square(double y) {
+        return y * y;
+    }
 
-        }
-
-
-    static void square(int a, double b, double c) {
-
-        }
-
+    static double square(double y, double z) {
+        return y * z;
+    }
 
 }
 
