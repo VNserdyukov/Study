16e96f47e4efb4d47ff18bbf76651c017fb5847e
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-04-26 23:04:20 +0300

    Lesson22Switch04

---
+++ src/Module4/Lesson41Massive03.java
@@ -0,0 +1,31 @@
+package Module4;
+
+import java.util.Scanner;
+
+public class Lesson41Massive03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] a = new int[scan.nextInt()];
+        int imin = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = scan.nextInt();
+            if (a[i] <= a[imin]) {
+                imin = i;
+            }
+        }
+
+        a[imin] = -1;
+
+        for (int el: a) {
+            System.out.print(el + " ");
+        }
+
+    }
+}
+
+
+
+
+
+
