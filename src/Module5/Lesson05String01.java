46f042d4a0683e18b749938cbba60f0ceb83569e
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-05-04 21:29:36 +0300

    Lesson22Switch04

---
+++ src/Module5/Lesson05String01.java
@@ -0,0 +1,15 @@
+package Module5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lesson05String01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] nov = {scan.nextLine(), scan.nextLine(), scan.nextLine()};
+        Arrays.sort(nov);
+        for (String el:nov) {
+            System.out.println(el);
+        }
+    }
+}
