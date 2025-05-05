a17fb4851306f3676d86f2eddc2dc2cfbb44b684
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-05-05 22:01:20 +0300

    Lesson22Switch04

--- src/Module5/Lesson05String06.java
+++ src/Module5/Lesson05String06.java
@@ -1,4 +1,24 @@
 package Module5;
 
+import java.util.Scanner;
+
 public class Lesson05String06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.nextLine();
+        System.out.println(max(a));
+    }
+
+    public static String max(String a) {
+        String[] mas = a.split(" +");
+        String word = "";
+        for (String el:mas) {
+            if (word.length() < el.length()) {
+                word = el;
+            }
+        }
+        return word;
+    }
+
 }
+
