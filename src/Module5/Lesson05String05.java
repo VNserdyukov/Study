ee314b74bbdddf4c01fc3b5f7bfd46fe73b88427
Victor <vn.serdyukov@mail.ru>
2025-05-05 15:06:16 +0300

    Study1

---
+++ src/Module5/Lesson05String05.java
@@ -0,0 +1,24 @@
+package Module5;
+
+import java.util.Scanner;
+
+public class Lesson05String05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.println(delCom(str));
+        System.out.println(delComRegular(str));
+    }
+
+    public static String delCom (String a) {
+        int ind = a.lastIndexOf(".com");
+        if (ind == -1) return a;
+        if (ind + ".com".length() != a.length()) return a;
+        return a.substring(0, ind);
+    }
+
+    public static String delComRegular(String str) {
+        return str.replaceAll("\\.com$", "");
+    }
+
+}
