a45e49278b94f57c485fe2fb213dd78fb46f4413
Victor <vn.serdyukov@mail.ru>
2025-05-05 13:35:22 +0300

    Study1

--- src/Training.java
+++ src/Training.java
@@ -1,12 +1,20 @@
-import java.util.Scanner;
-
 public class Training {
     public static void main(String[] args) {
-        Scanner scan  = new Scanner(System.in);
-        int number = scan.nextInt();
-        int num1 = number / 100;
-        int num2 = (number / 10) % 10;
-        int num3 = number % 10;
-        System.out.println(num1);
+        String sample = "   Testing strings   ";
+        sample = sample.trim(); //удаление пробелов в начале и в конце
+        System.out.println("|" + sample + "|");
+        System.out.println(sample.charAt(3)); //символ с индексом 3 - это t
+        System.out.println(sample.length());
+        System.out.println(sample.charAt(sample.length() - 1)); //последний символ - это s
+        String sub = sample.substring(3, 7); //выделение подстроки ting
+        System.out.println(sub);
+        String changed = sample.replace('s', 'z'); //замена символов s на z
+        System.out.println(changed);
+        changed = changed.replace("z", ""); //удаление символов z
+        System.out.println(changed);
+        int findFirst = sample.indexOf('i'); //поиск символа с начала строки
+        System.out.println(findFirst); //выводит 4
+        int findLast = sample.lastIndexOf('i'); //поиск символа с конца строки
+        System.out.println(findLast); //выводит 11
     }
 }
