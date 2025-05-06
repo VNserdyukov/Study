c5c486d5bf4cdc6a115f76e069ca4a07eae21580
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-05-07 00:10:06 +0300

    Lesson22Switch04

---
+++ src/Module5/Lesson51StringBuffer02.java
@@ -0,0 +1,24 @@
+package Module5;
+
+import java.util.Scanner;
+
+public class Lesson51StringBuffer02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String data = scan.nextLine();
+        String[] words = data.split(" +");
+        StringBuilder sb = new StringBuilder(words[0]);
+        while (data.length() != sb.length()) {
+            for (int i = 1; i < words.length; i++) {
+                if (sb.charAt(sb.length() - 1) == words[i].charAt(0)) {
+                    sb.append(" ").append(words[i]);
+                    break;
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
+
+
+
