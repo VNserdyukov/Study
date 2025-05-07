37b4b2f890d1766cf61a26937b17652248ac6543
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-05-07 22:42:10 +0300

    Lesson22Switch04

--- src/Module5/Lesson51StringBuffer04.java
+++ src/Module5/Lesson51StringBuffer04.java
@@ -6,14 +6,40 @@ public class Lesson51StringBuffer04 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String a = scan.nextLine();
-        String[] words = a.split("\\b");
-        System.out.println(words);
+        String[] words = a.split("\\D+");
+        if (words.length < 1) {
+            System.out.println("ERROR");
+            return;
+        }
+        StringBuilder sbwords = new StringBuilder();
+        for (String el:words) {
+            sbwords.append(el);
+        }
+
+        String b = sbwords.toString();
+        String[] numbers = b.split("");
+        StringBuilder sbnums = new StringBuilder();
+        int sum = 0;
+        for (String elnum : numbers) {
+            sbnums.append(elnum).append("+");
+            sum += Integer.parseInt(elnum);
+        }
+        sbnums.deleteCharAt(sbnums.length() - 1);
+        System.out.println(sbnums + "=" + sum);
     }
-
-//    public static String primer(int a) {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append(a).append(" + ").append(b).append(" = ").append(a + b);
-//        return sb.toString();
-//    }
-
 }
+
+//        for (int i = 0; i < str.length(); i++) {
+//        if (str.charAt(i) >= '0' &&  str.charAt(i) <= '9'){
+//        summa += str.charAt(i) - '0';
+//        result.append(str.charAt(i)).append('+');
+//          }
+//        }
+//        if (!result.isEmpty()) {
+//        result.replace(result.length() - 1, result.length(), "=");
+//        result.append(summa);
+//            System.out.println(result);
+//        } else {
+//            System.out.println("ERROR");
+//          }
+//        }
