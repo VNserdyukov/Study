0e977de82e483e9abbf7797f7e20850b90508a4a
Victor <vn.serdyukov@mail.ru>
2025-05-11 16:45:42 +0300

    Study1

--- src/OOP/Module1/Abiturient.java
+++ src/OOP/Module1/Abiturient.java
@@ -1,4 +1,18 @@
 package OOP.Module1;
 
 public class Abiturient {
+    String surname;
+    String name;
+    int math;
+    int physics;
+    int lang;
+
+    double average() {
+        return (math + physics + lang) / 3.;
+    }
+
+    void print() {
+        System.out.printf("%s %s математика = %d физика = %d", surname, name, math, physics);
+        System.out.printf(" язык = %d средний балл = %.1f", lang, average());
+    }
 }
