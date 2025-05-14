a912ef90fb9d846550632eb53711f05bf0150f8e
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-05-14 22:11:30 +0300

    Lesson22Switch04

--- src/OOP/Module1/Student/Abiturient.java
+++ src/OOP/Module1/Student/Abiturient.java
@@ -1,11 +1,67 @@
 package OOP.Module1.Student;
 
 public class Abiturient {
-    String surname;
-    String name;
-    int math;
-    int physics;
-    int lang;
+    private String surname;
+    private String name;
+    private int math;
+    private int physics;
+    private int lang;
+
+    public Abiturient(String surname, String name, int math, int physics, int lang) {
+        this.lang = lang;
+        this.physics = physics;
+        this.math = math;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Abiturient() {
+    }
+
+    public Abiturient(String surname, String name) {
+        this.surname = surname;
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMath() {
+        return math;
+    }
+
+    public void setMath(int math) {
+        this.math = math;
+    }
+
+    public int getPhysics() {
+        return physics;
+    }
+
+    public void setPhysics(int physics) {
+        this.physics = physics;
+    }
+
+    public int getLang() {
+        return lang;
+    }
+
+    public void setLang(int lang) {
+        this.lang = lang;
+    }
 
     double average() {
         return (math + physics + lang) / 3.;
--- src/OOP/Module1/Student/Main.java
+++ src/OOP/Module1/Student/Main.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Abiturient person = new Abiturient();
-        person.surname = scan.next();
-        person.name = scan.next();
-        person.math = scan.nextInt();
-        person.physics = scan.nextInt();
-        person.lang = scan.nextInt();
-        System.out.printf("%.1f\n", person.average());
-        person.print();
+        Abiturient[] list = new Abiturient[3];
+        list[0] = new Abiturient();
+        list[1] = new Abiturient("Петров", "Андрей", 90, 70, 80);
+        list[2] = new Abiturient("Алексиевич", "Анна");
+        list[0].setSurname(scan.next());
+        list[2].setPhysics(scan.nextInt());
+        list[2].setMath(scan.nextInt());
+        list[2].setLang(scan.nextInt());
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null) {
+                System.out.printf("%s : %.1f\n", list[i].getSurname(), list[i].average());
+            }
+        }
     }
 }
 
