3c6d5c1f24e75e88c808a29084bfcf47b20f2af2
Victor <vn.serdyukov@mail.ru>
2025-05-20 16:56:50 +0300

    Study1

---
+++ src/OOP/Module1/Book/Book.java
@@ -0,0 +1,19 @@
+package OOP.Module1.Book;
+
+public class Book {
+    private String name;
+    private String author;
+    private String publisher;
+    private int age;
+
+    public Book() {
+    }
+
+    public Book(String name, String author, String publisher, int age) {
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+        this.age = age;
+    }
+
+}
