9b97938a6dd4b7581619061f6ef8a9a6ba2acd57
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-05-20 23:13:16 +0300

    Lesson22Switch04

--- src/OOP/Module1/Book/Book.java
+++ src/OOP/Module1/Book/Book.java
@@ -4,20 +4,39 @@ public class Book {
     private String name;
     private String author;
     private String publisher;
-    private int age;
+    private int year;
 
     public Book() {
-        this.name = ";";
-        this.author = ";";
-        this.publisher = ";";
-        this.age = 0;
+        this.name = "";
+        this.author = "";
+        this.publisher = "";
+        this.year = 0;
     }
 
-    public Book(String name, String author, String publisher, int age) {
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+        this.publisher = "";
+        this.year = 0;
+    }
+
+    public Book(String name, String author, String publisher) {
         this.name = name;
         this.author = author;
         this.publisher = publisher;
-        this.age = age;
+        this.year = 0;
+    }
+
+    public Book(String name, String author, String publisher, int year) {
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return name + ';' + author + ';' + publisher + ';' + year;
     }
 
 }
