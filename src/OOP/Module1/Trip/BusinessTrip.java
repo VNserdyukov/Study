ebe8da8d4335a8c68f99cf6d1695a8fbcf9cd3ef
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-05-21 23:50:49 +0300

    Lesson22Switch04

--- src/OOP/Module1/Trip/BusinessTrip.java
+++ src/OOP/Module1/Trip/BusinessTrip.java
@@ -1,21 +1,62 @@
 package OOP.Module1.Trip;
 
-public class BusinessTrip {
-    static final int DAILY = 25;
-    String name;
-    int expenses;
-    int days;
+import java.util.Objects;
 
-    int getTotal() {
+public class BusinessTrip {
+    private static final int DAILY = 25;
+    private String name;
+    private int days;
+    private int expenses;
+
+
+    public int getTotal() {
         return expenses + days * DAILY;
     }
 
-    void show() {
-        System.out.println("Суточные = " + DAILY);
-        System.out.println("Фамилия Имя = " + name);
-        System.out.println("Транспортные расходы = " + expenses);
-        System.out.println("Количество дней = " + days);
-        System.out.println("Итого расходы = " + getTotal());
+    public BusinessTrip(String name) {
+        this.name = name;
+    }
+
+    public BusinessTrip(String name, int days) {
+        this.name = name;
+        this.expenses = days;
+    }
+
+    public BusinessTrip(String name, int days, int expenses) {
+        this.name = name;
+        this.days = days;
+        this.expenses = expenses;
+    }
+
+    public BusinessTrip(BusinessTrip trip) {
+        this.name = trip.name;
+        this.expenses = trip.expenses;
+        this.days = trip.days;
+    }
+
+    public BusinessTrip() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return name + ';' + days + ';' + expenses + ';' + getTotal();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BusinessTrip that = (BusinessTrip) o;
+        return this.getTotal() == that.getTotal();
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(getTotal());
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
 }
--- src/OOP/Module1/Trip/Main.java
+++ src/OOP/Module1/Trip/Main.java
@@ -5,10 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
-        BusinessTrip worker = new BusinessTrip();
-        worker.name = scan.nextLine();
-        worker.days = scan.nextInt();
-        worker.expenses = scan.nextInt();
-        worker.show();
+        int mas = scan.nextInt();
+        if (mas < 0) {
+            System.out.println("ERROR");
+            return;
+        }
+
+        BusinessTrip[] workers = new BusinessTrip[mas];
+        for (int i = 0; i < workers.length - 1; i++) {
+            workers[i] = new BusinessTrip(scan.next(), scan.nextInt(), scan.nextInt());
+        }
+        workers[workers.length - 1] = new BusinessTrip(workers[0]);
+        workers[0].setName(scan.next());
+        for (BusinessTrip worker : workers) {
+            System.out.println(worker);
+        }
+
+        int index = scan.nextInt();
+        for (BusinessTrip worker : workers) {
+            if (worker.equals(workers[index])) {
+                System.out.println(worker);
+            }
+        }
+
     }
 }
