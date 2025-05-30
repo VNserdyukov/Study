c113fe3f2640a0170ff7cf509e3db00ab63b32b2
Victor <vn.serdyukov@mail.ru>
2025-05-30 15:15:35 +0300

    Study1

---
+++ src/OOP/Module2/People/Employee.java
@@ -0,0 +1,33 @@
+package OOP.Module2.People;
+
+public class Employee {
+    private String surname;
+    private String gender;
+    private int age;
+    private int salary;
+    private double koef;
+
+    public Employee(String surname, String gender, int age, int salary) {
+        this.surname = surname;
+        this.gender = gender;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    boolean isPensioner() {
+        if (gender.equals("male") && age >= 63) return true;
+        return gender.equals("female") && age >= 58;
+    }
+
+    void changeSalary(double koef) {
+        salary = (int) (salary * koef);
+    }
+
+    void show() {
+        System.out.println(surname + ";" + gender + ';' + age + ";" + salary);
+    }
+}
---
+++ src/OOP/Module2/People/Main.java
@@ -0,0 +1,29 @@
+package OOP.Module2.People;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int mas = scan.nextInt();
+        if (mas < 0) {
+            System.out.println("ERROR");
+            return;
+        }
+
+        Employee[] worker = new Employee[mas];
+        for (int i = 0; i < worker.length; i++) {
+            worker[i] = new Employee(scan.next(), scan.next(), scan.nextInt(), scan.nextInt());
+            if (worker[i].isPensioner()) {
+                worker[i].setSalary(90000);
+            }
+        }
+
+        for (Employee person:worker) {
+            if (person != null) {
+                person.show();
+            }
+        }
+
+    }
+}
