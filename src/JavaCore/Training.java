948fd5aa68e7c732b42dfd9d824cd85f1831acce
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-06-18 21:32:13 +0300

    Lesson22Switch04

--- src/JavaCore/Training.java
+++ src/JavaCore/Training.java
@@ -1,22 +1,21 @@
 package JavaCore;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Training {
     public static void main(String[] args) {
-        String sample = "   Testing strings   ";
-        sample = sample.trim(); //удаление пробелов в начале и в конце
-        System.out.println("|" + sample + "|");
-        System.out.println(sample.charAt(3)); //символ с индексом 3 - это t
-        System.out.println(sample.length());
-        System.out.println(sample.charAt(sample.length() - 1)); //последний символ - это s
-        String sub = sample.substring(3, 7); //выделение подстроки ting
-        System.out.println(sub);
-        String changed = sample.replace('s', 'z'); //замена символов s на z
-        System.out.println(changed);
-        changed = changed.replace("z", ""); //удаление символов z
-        System.out.println(changed);
-        int findFirst = sample.indexOf('i'); //поиск символа с начала строки
-        System.out.println(findFirst); //выводит 4
-        int findLast = sample.lastIndexOf('i'); //поиск символа с конца строки
-        System.out.println(findLast); //выводит 11
+        Scanner scan = new Scanner(System.in);
+        int[] array = null;
+        try {
+            int n = scan.nextInt();
+            array = new int[n];
+            array[n] = n;
+        } catch (NegativeArraySizeException ex) {
+            System.out.println("Error1");
+        } catch (InputMismatchException ex) {
+            System.out.println("Error2");
+        } catch (Exception ex) {
+            System.out.println("Error3");
+        }
     }
 }
