86bd35b23d8f2b3d0e10373759ab3ac5389e02ad
Victor <vn.serdyukov@mail.ru>
2025-06-19 11:29:32 +0300

    Study1

---
+++ src/OOP/Module2/Exeption1RandomMassive/Main.java
@@ -0,0 +1,21 @@
+package OOP.Module2.Exeption1RandomMassive;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random(scan.nextLong());
+        int a = random.nextInt(-10,11);
+        try {
+            int[] mas = new int[a];
+            for (int i = 0; i < mas.length; i++) {
+                mas[i] = i;
+            }
+            System.out.println(Arrays.toString(mas));
+        } catch (NegativeArraySizeException ex) {
+            System.out.println("Неверный размер массива: " + a);
+        }
+    }
+}
