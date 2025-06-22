e011adac4014f8ba583dbcd5d3d13409f3dc4b25
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-06-22 21:58:34 +0300

    Lesson22Switch04

---
+++ src/OOP/Module2/Exception2Baking/BakingException.java
@@ -0,0 +1,9 @@
+package OOP.Module2.Exception2Baking;
+
+public class BakingException extends Exception {
+
+    public BakingException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/OOP/Module2/Exception2Baking/Main.java
@@ -0,0 +1,20 @@
+package OOP.Module2.Exception2Baking;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String product = scanner.nextLine();
+        Cook dish = new Cook();
+        try {
+            dish.turnOvenOn();
+            dish.bake(product);
+            System.out.println("Блюдо готово");
+        } catch (BakingException ex) {
+            System.out.println("Блюдо полностью испортилось: " + ex.getMessage());
+        } finally {
+            dish.turnOvenOff();
+        }
+    }
+}
