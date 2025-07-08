1373fe171136a315a349c1dc830d3726876cf355
Victor <vn.serdyukov@mail.ru>
2025-07-08 16:56:36 +0300

    Study1

--- src/OOP/Module4/Jeneric/Jeneric2Lists/Main.java
+++ src/OOP/Module4/Jeneric/Jeneric2Lists/Main.java
@@ -1,4 +1,22 @@
 package OOP.Module4.Jeneric.Jeneric2Lists;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> studentYoung = new ArrayList<>();
+        ArrayList<String> studentOld = new ArrayList<>();
+
+        while (true) {
+            String buffer = scan.nextLine();
+            if (buffer.equals("end")) break;
+            String[] parts = buffer.split(" ");
+            double average = Double.parseDouble(parts[1]);
+            int age = Integer.parseInt(parts[2]);
+            Student student = new Student(parts[0], average, age);
+            if (average >= 18)
+        }
+    }
 }
