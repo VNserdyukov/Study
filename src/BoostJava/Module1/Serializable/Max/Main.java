b4abded0480b15c77e362d3c7ddae35033b04c49
Виктор Николаевич <vn.serdyukov@mail.ru>
2025-08-02 20:24:53 +0300

    Lesson22Switch04

--- src/BoostJava/Module1/Serializable/Max/Main.java
+++ src/BoostJava/Module1/Serializable/Max/Main.java
@@ -1,9 +1,25 @@
 package BoostJava.Module1.Serializable.Max;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<Integer> list = null;
+        try(ObjectInputStream is = new ObjectInputStream(
+                new FileInputStream("./src/BoostJava/Module1/Serializable/Max/list.ser"))){
+            list = (ArrayList<Integer>) is.readObject(); //читаем объект из файла
+        }catch(IOException ex){
+            System.out.println("Ошибка чтения: " + ex.getMessage());
+        }catch(ClassNotFoundException ex){
+            System.out.println("Не найден файл для десериализации" + ex.getMessage());
+        }
+        int max = list.get(0);
+        for (Integer item : list) {
+            if (item > max) {
+                max = item;
+            }
+        }
+        System.out.println(max);
     }
 }
--- src/BoostJava/Module1/Serializable/Middle/Main.java
+++ src/BoostJava/Module1/Serializable/Middle/Main.java
@@ -1,4 +1,24 @@
 package BoostJava.Module1.Serializable.Middle;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class Main {
+    public static void main(String[] args) {
+        double[] mas = null;
+        try(ObjectInputStream is = new ObjectInputStream(
+                new FileInputStream("./src/BoostJava/Module1/Serializable/Middle/massiv.ser"))){
+            mas =(double[]) is.readObject(); //читаем объект из файла
+        }catch(IOException ex){
+            System.out.println("Ошибка чтения: " + ex.getMessage());
+        }catch(ClassNotFoundException ex){
+            System.out.println("Не найден файл для десериализации" + ex.getMessage());
+        }
+        double sum = 0;
+        for (double x : mas) {
+            sum += x;
+        }
+        System.out.printf("%.2f", sum / mas.length);
+    }
 }
