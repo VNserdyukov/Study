0e8eef5e061c2c7022e32d853bae7f5dc6bd89ed
Victor <vn.serdyukov@mail.ru>
2025-08-21 13:25:22 +0300

    Study

--- src/BoostJava/Module2/Map/Repeat/Main.java
+++ src/BoostJava/Module2/Map/Repeat/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Map<Integer, Integer> freq = new HashMap<>();
-
         while (sc.hasNext()) {
             String token = sc.next();
             if (token.equals("end")) break;
