86cfb41c807e874d3bb75eb23a5372ed65f2e98c
Victor <vn.serdyukov@mail.ru>
2025-08-28 16:54:56 +0300

    Study

--- src/BoostJava/Module5/ReadWords/Job.java
+++ src/BoostJava/Module5/ReadWords/Job.java
@@ -1,10 +1,37 @@
 package BoostJava.Module5.ReadWords;
 
-public class Job implements  Runnable{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Job implements Runnable {
+    private BufferedReader br;
+    private ArrayList<String> list = new ArrayList<>();
+
+    public Job(BufferedReader br) {
+        this.br = br;
+    }
+
+    public ArrayList<String> getList() {
+        return list;
+    }
 
     @Override
     public void run() {
 
+        try {
+            String str = br.readLine();
+            while(str != null){
+                list.add(str);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                str = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/BoostJava/Module5/ReadWords/Main.java
+++ src/BoostJava/Module5/ReadWords/Main.java
@@ -1,4 +1,37 @@
 package BoostJava.Module5.ReadWords;
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new FileReader("./src/BoostJava/Module5/ReadWords/data.txt"))){
+            Job one = new Job(br);
+            Job two = new Job(br);
+            Job three = new Job(br);
+            Thread t1 = new Thread(one,"first");
+            Thread t2 = new Thread(two,"second");
+            Thread t3 = new Thread(three,"third");
+            t1.start();
+            t2.start();
+            t3.start();
+            try {
+                t1.join();
+                t2.join();
+                t3.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Первый поток считал: " + one.getList());
+            System.out.println("Второй поток считал: " + two.getList());
+            System.out.println("Третий поток считал: " + three.getList());
+        }catch(FileNotFoundException e){
+            System.out.println("Файл не найден");
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
 }
