2b55e7fba6c1542b1726c99fd0739ec43690d4b5
Victor <vn.serdyukov@mail.ru>
2025-09-11 11:29:08 +0300

    Study

--- src/BoostJava/Module6/StandartFunctionalInterface/FindBuys/Main.java
+++ src/BoostJava/Module6/StandartFunctionalInterface/FindBuys/Main.java
@@ -1,10 +1,10 @@
 package BoostJava.Module6.StandartFunctionalInterface.FindBuys;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.function.Predicate;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 class Main {
     public static void main(String[] args) {
@@ -16,13 +16,22 @@ class Main {
             list.add(new Purchase(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
             str = scan.nextLine();
         }
-
+        Supplier<Purchase> supplier = () -> new Purchase("Покупка не найдена", 0, 0);
+        Optional<Purchase> optional = findFirst(list, p -> p.getName().startsWith("М"));
+        System.out.println("Первая покупка на букву М: " + optional.orElseGet(supplier));
+        Optional<Purchase> optionalCost = findFirst(list, p -> p.getCost() > 1000);
+        System.out.println("Первая покупка со стоимостью больше 1000: " + optionalCost.orElseGet(supplier));
     }
-    public static void findFirst(List<Purchase> list, Predicate<Purchase> purchases) {
+
+    public static Optional<Purchase> findFirst(ArrayList<Purchase> list, Predicate<Purchase> purchases) {
+        Purchase result = null;
         for (Purchase purchase : list) {
             if (purchases.test(purchase)) {
-                System.out.println(purchase);
+                result = purchase;
+                break;
             }
         }
+        return Optional.ofNullable(result);
     }
+
 }
