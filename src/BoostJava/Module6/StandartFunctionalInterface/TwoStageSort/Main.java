e026c2fbaef17b60be465a30f67614052f34d2d2
Victor <vn.serdyukov@mail.ru>
2025-09-12 15:24:42 +0300

    Study

--- src/BoostJava/Module6/StandartFunctionalInterface/TwoStageSort/Main.java
+++ src/BoostJava/Module6/StandartFunctionalInterface/TwoStageSort/Main.java
@@ -2,7 +2,6 @@ package BoostJava.Module6.StandartFunctionalInterface.TwoStageSort;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Collections;
 import java.util.Comparator;
 
 public class Main {
@@ -15,6 +14,10 @@ public class Main {
             list.add(new Purchase(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2])));
             str = scan.nextLine();
         }
-
+        Comparator<Purchase> comparatorName = Comparator.comparing(Purchase::getName);
+        Comparator<Purchase> comparatorCost = comparatorName.thenComparing(purchase -> -purchase.getCost());
+        // можно использовать мину "-" перед purchase.getCost()) для сортировки по убыванию
+        list.sort(comparatorCost);
+        list.forEach(System.out::println);
     }
 }
