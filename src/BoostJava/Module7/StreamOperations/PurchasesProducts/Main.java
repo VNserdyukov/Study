8f902923ef14ad46ad237c73a693fe92b9cb4811
Victor <vn.serdyukov@mail.ru>
2025-10-16 11:55:43 +0300

    Study

--- src/BoostJava/Module7/StreamOperations/PurchasesProducts/Main.java
+++ src/BoostJava/Module7/StreamOperations/PurchasesProducts/Main.java
@@ -1,9 +1,6 @@
 package BoostJava.Module7.StreamOperations.PurchasesProducts;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
