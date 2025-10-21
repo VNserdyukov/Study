e05a028f49b743f34ce18e322be9f76216366c6f
Victor <vn.serdyukov@mail.ru>
2025-10-21 16:51:11 +0300

    Study

--- src/BoostJava/Module7/CollectorsAndComaparator/StudentLists2/Student.java
+++ src/BoostJava/Module7/CollectorsAndComaparator/StudentLists2/Student.java
@@ -1,4 +1,30 @@
 package BoostJava.Module7.CollectorsAndComaparator.StudentLists2;
 
 public class Student {
+    private String name;
+    private String gruppa;
+    private int age;
+
+    public Student(String name, String gruppa, int age) {
+        this.name = name;
+        this.gruppa = gruppa;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGruppa() {
+        return gruppa;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ';' + age;
+    }
 }
